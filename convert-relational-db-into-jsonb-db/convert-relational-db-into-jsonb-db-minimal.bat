6816e9dc5b157b919e0e1141acf504f08a5f9e18
Vladimir Brazhnik <v.brazhnik@web.de>
2021-06-01 19:39:29 +0200

    Create separeted scripts for table convertion in separated folder

---
+++ convert-relational-db-into-jsonb-db/convert-relational-db-into-jsonb-db-minimal.bat
@@ -0,0 +1,5 @@
+echo "Converting relational customers data table into jsonb customers_jsonb data table..."
+
+set PGPASSWORD=postgres
+psql -U postgres -d ds2_minimal -f convert-relational-db-into-jsonb-db.sql
+pause
